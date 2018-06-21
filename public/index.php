1fb6774e85e76073d87e4cf861e6a57cd0cf4f13
zhaokai <zk@pcuu.com>
2018-06-21 15:30:31 +0800

    DI

--- public/index.php
+++ public/index.php
@@ -1,6 +1,23 @@
 <?php
 declare(strict_types=1);
 
+use DI\ContainerBuilder;
+use ExampleApp\HelloWorld;
+use function DI\create;
+
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-echo 'Hello, world!';
+// $helloWorld = new \ExampleApp\HelloWorld();
+// $helloWorld->announce();
+
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->useAutowiring(false);
+$containerBuilder->useAnnotations(false);
+$containerBuilder->addDefinitions([
+    HelloWorld::class => create(HelloWorld::class)
+]);
+
+$container = $containerBuilder->build();
+
+$helloWorld = $container->get(HelloWorld::class);
+$helloWorld->announce();
